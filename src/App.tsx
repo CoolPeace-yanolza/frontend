03555f6514634619d6ef9609d57d311b97bc74ab
JitHoon <imessi0336@gmail.com>
2024-01-01 12:14:02 +0900

    design: emotion props, 팀 css 컨벤션 적용 예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [primary, setPrimary] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <EmotionExampleWrapper>
+      <EmotionExample
+        $primary={primary}
+        onClick={() => setPrimary((prev) => !prev)}
+      >
+        쿨피스 응원 버튼
+        <br />
+        (emotion props 사용 예시)
+      </EmotionExample>
+    </EmotionExampleWrapper>
+  );
+};
 
-export default App
+export default App;
+
+const EmotionExampleWrapper = styled.div`
+  /* 0. emotipn 변수 */
+  /* 1. position */
+  /* 2. width / height */
+  width: 100vw;
+  height: 100vh;
+
+  /* 3. margin / border / padding */
+  /* 4. display */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* 나머지 디자인 */
+  /* 5. 인터렉션 */
+  /* 6. props에 따른 스타일 */
+`;
+
+const EmotionExample = styled.button<{ $primary?: boolean }>`
+  /* 0. emotipn 변수 */
+  --accent-color: white;
+
+  /* 1. position */
+
+  /* 2. width / height */
+  width: 300px;
+  height: 90px;
+
+  /* 3. margin / border / padding */
+  margin: 0.5rem 1rem;
+  border-radius: 3px;
+  border: 1px solid var(--accent-color);
+  padding: 5px;
+
+  /* 4. display */
+  display: inline-block;
+
+  /* 나머지 디자인 */
+  color: var(--accent-color);
+  background: transparent;
+
+  transition: all 200ms ease-in-out;
+
+  /* 5. 인터렉션 */
+  &:hover {
+    filter: brightness(0.85);
+  }
+
+  &:active {
+    filter: brightness(1);
+  }
+
+  /* 6. props에 따른 스타일 */
+  ${(props) =>
+    props.$primary &&
+    css`
+      background: var(--accent-color);
+      color: black;
+    `}
+`;
