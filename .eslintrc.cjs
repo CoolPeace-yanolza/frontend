2518aca90c6f6429b6999f78e40d76280aed9e93
JitHoon <imessi0336@gmail.com>
2024-01-01 13:04:52 +0900

    chore: eslint, prettier 충돌 방지 패키지 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
