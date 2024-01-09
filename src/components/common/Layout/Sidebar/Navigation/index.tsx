8b0cd3f0b715ecd00bb924cf3826e95d7419f73d
JitHoon <imessi0336@gmail.com>
2024-01-09 15:18:09 +0900

    feat: 공통 사이드바 네비게이션 (메뉴) 구현

---
+++ src/components/common/Layout/Sidebar/Navigation/index.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+import Coupon from './Coupon';
+import Settlements from './Settlements';
+
+const Navigation = ({ isSidebarOpen }: { isSidebarOpen: boolean }) => {
+  return (
+    <Container $isSidebarOpen={isSidebarOpen}>
+      <Coupon isSidebarOpen={isSidebarOpen} />
+      <Settlements isSidebarOpen={isSidebarOpen} />
+    </Container>
+  );
+};
+export default Navigation;
+
+const Container = styled.nav<{ $isSidebarOpen?: boolean }>`
+  width: ${props => (props.$isSidebarOpen ? '100%' : '5rem')};
+
+  display: flex;
+  flex-direction: column;
+
+  font-size: ${props => (props.$isSidebarOpen ? '0.875rem' : '0.75rem')};
+`;
