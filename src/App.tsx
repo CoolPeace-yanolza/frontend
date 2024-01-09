64e014535696e6169229d68f5926d0223c66d370
JitHoon <imessi0336@gmail.com>
2024-01-10 02:12:31 +0900

    chore: tanstack-query (devtools, eslint) 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import GlobalStyle from '@styles/GlobalStyle';
-import { ThemeProvider } from '@emotion/react';
-import theme from '@styles/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { MainRouter } from './routes';
+import GlobalStyle from '@styles/GlobalStyle';
+import theme from '@styles/theme';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <MainRouter />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <MainRouter />
+          </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
