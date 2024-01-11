371ec73f453054f57e054a2ab00b92712a045279
JitHoon <imessi0336@gmail.com>
2024-01-11 17:57:59 +0900

    design: select 태그 스타일

--- src/components/common/Layout/Header/index.tsx
+++ src/components/common/Layout/Header/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
 
@@ -6,21 +5,9 @@ import logo from '@assets/icons/ic-logo.svg';
 // HACK: 디자이너에게 유저 아이콘 다시 받을 예정
 import user from '@assets/icons/ic-header-user.svg';
 import { toRem } from '@utils/index';
+import Select from './Select';
 
 const Header = () => {
-  // HACK: 예시 데이터, 백엔드에 리스트로 넘겨 받기
-  const selectList = [
-    '영덕 아이스 풀빌라',
-    '영덕 아이스 풀빌라2',
-    '영덕 아이스 풀빌라3',
-    '영덕 아이스 풀빌라4'
-  ];
-  const [selected, setSelected] = useState(selectList[0]);
-
-  // HACK: select 값에 따른 API 요청을 어떻게 보낼 건지 논의 필요
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) =>
-    setSelected(e.target.value);
-
   return (
     <Container>
       <LogoLink
@@ -33,19 +20,7 @@ const Header = () => {
         />
       </LogoLink>
       <Buttons>
-        <Accommodations
-          onChange={handleSelect}
-          value={selected}
-        >
-          {selectList.map(item => (
-            <option
-              value={item}
-              key={item}
-            >
-              {item}
-            </option>
-          ))}
-        </Accommodations>
+        <Select />
 
         <UserIcon
           src={user}
@@ -85,10 +60,6 @@ const LogoIcon = styled.img`
 
 const Buttons = styled.div`
   display: flex;
-`;
-
-const Accommodations = styled.select`
-  margin-right: ${toRem(32)};
 `;
 
 const UserIcon = styled.img`
