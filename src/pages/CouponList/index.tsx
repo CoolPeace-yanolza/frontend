c8e9688a9a1ccbad5d785b7275a70513aee1a268
jinjoo-jung <wjdwlswn23@naver.com>
2024-01-11 17:38:15 +0900

    feat: 쿠폰 노출 중지 구현

--- src/pages/CouponList/index.tsx
+++ src/pages/CouponList/index.tsx
@@ -1,10 +1,12 @@
-import CouponItem from '@components/CouponList/CouponItem';
+import CouponItem from '@components/CouponList/CouponItem/CouponExpose';
+import CouponStop from '@components/CouponList/CouponItem/CouponStop';
 import React from 'react';
 
 const CouponList = () => {
   return (
     <div>
       <CouponItem />
+      <CouponStop />
     </div>
   );
 };
