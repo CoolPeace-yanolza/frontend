08e8093611a4c601019ab52b1369ff15e1bbb197
jiohjung98 <ckaquz98@naver.com>
2024-01-12 18:04:28 +0900

    design: 페이지네이션 디자인 작업

--- src/components/Settlements/SettlementsLeft/Settlemented/SettlementsPagination/index.tsx
+++ src/components/Settlements/SettlementsLeft/Settlemented/SettlementsPagination/index.tsx
@@ -1,44 +1,98 @@
+import React, { useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import styled from '@emotion/styled';
 
-interface PageNumberProps {
-    isActive: boolean;
-  }
-
 interface SettlementsPaginationProps {
-    currentPage: number;
-    totalPages: number;
-    onPageChange: (page: number) => void;
-  }
-  
-  const SettlementsPagination: React.FC<SettlementsPaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
-  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const SettlementsPagination: React.FC<SettlementsPaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange
+}) => {
+  useEffect(() => {
+    onPageChange(currentPage);
+  }, [currentPage, onPageChange]);
 
   return (
     <PaginationContainer>
-      {pages.map((page) => (
-        <PageNumber
-          key={page}
-          onClick={() => onPageChange(page)}
-          isActive={page === currentPage}
-        >
-          {page}
-        </PageNumber>
-      ))}
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        breakLabel={'...'}
+        breakClassName={'hidden'}
+        pageCount={totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={(selectedItem) => onPageChange(selectedItem.selected + 1)}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+        pageClassName={'pagination-li'}
+        pageLinkClassName={'pagination-link'}
+        previousClassName={'pagination-previous'}
+        nextClassName={'pagination-next'}
+      />
     </PaginationContainer>
   );
 };
 
+export default SettlementsPagination;
+
 const PaginationContainer = styled.div`
   margin-top: 20px;
+  margin-bottom: 20px;
+
   display: flex;
   justify-content: center;
-  gap: 10px;
-`;
+  align-items: center;
 
-const PageNumber = styled.div<PageNumberProps>`
-  cursor: pointer;
-  font-weight: ${({ isActive }) => (isActive ? 'bold' : 'normal')};
-  color: ${({ isActive }) => (isActive ? 'blue' : 'black')};
-`;
+  .pagination {
+    display: flex;
+    align-items: center;
+  }
 
-export default SettlementsPagination;
+  .pagination-li {
+    margin: 5px;
+    cursor: pointer;
+  }
+
+  .pagination-link {
+    padding: 8px 12px;
+    border: 1px solid #FFFFFF;
+    border-radius: 4px;
+    color: #FFFFFF;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #f0f0f0;
+    }
+
+    &.active {
+      font-weight: bold;
+      background-color: #e0e0e0;
+    }
+  }
+
+  .pagination-previous,
+  .pagination-next {
+    padding: 8px 12px;
+
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    
+    color: #FFFFFF;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #f0f0f0;
+    }
+
+    &.active {
+      font-weight: bold;
+      background-color: #e0e0e0;
+    }
+  }
+`;
