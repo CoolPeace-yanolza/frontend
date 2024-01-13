672b2a5039cac934e9a8a95b459b7fe59820c2c1
jinjoo-jung <wjdwlswn23@naver.com>
2024-01-14 05:14:46 +0900

    feat: 최근 등록일 버튼 디자인 추가

--- src/components/CouponList/CouponNav/index.tsx
+++ src/components/CouponList/CouponNav/index.tsx
@@ -1,14 +1,30 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 import searchIcon from '@assets/icons/CouponList/ic_search.svg';
 import centerIcon from '@assets/icons/CouponList/ic_period_center.svg';
+import bannerIcon from '@assets/icons/CouponList/ic_banner.png';
 
 const CouponNav = () => {
+  const [isClick, setIsClick] = useState<string>('');
+
+  const handleDateClick = (period: string) => {
+    setIsClick(period);
+  };
   return (
     <TabContainer>
       <TabBanner>
-        현재 우리 숙소의 가장 인기있는 쿠폰은? 재방문 고객 20% 할인쿠폰 이에요!{' '}
+        <img
+          src={bannerIcon}
+          alt="bannerIcon"
+        />
+        <div>
+          <TabBannerTitle>이번 달 우리 지역 인기 쿠폰</TabBannerTitle>
+          <TabBannerContent>
+            OO구에서 가장 많이 사용된 쿠폰은? 재방문 고객 20% 할인쿠폰 이에요!
+          </TabBannerContent>
+        </div>
       </TabBanner>
+
       <TabNavContainer>
         <TabWrap>
           <TapItemWrapper>
@@ -51,18 +67,33 @@ const CouponNav = () => {
           </CouponDescription>
         </TabBottomWrap>
         <ResisterPeriodWrap>
-          <ResisterPeriod>등록일 기준 최근</ResisterPeriod>
-          <ResisterPeriod>1년</ResisterPeriod>
+          <ResisterPeriodTitle>등록일 기준 최근</ResisterPeriodTitle>
+          <ResisterPeriod
+            isClick={isClick === '1년'}
+            onClick={() => handleDateClick('1년')}
+          >
+            1년
+          </ResisterPeriod>
           <img
             src={centerIcon}
             alt="centerIcon"
           />
-          <ResisterPeriod>6개월</ResisterPeriod>
+          <ResisterPeriod
+            isClick={isClick === '6개월'}
+            onClick={() => handleDateClick('6개월')}
+          >
+            6개월
+          </ResisterPeriod>
           <img
             src={centerIcon}
             alt="centerIcon"
           />
-          <ResisterPeriod>3개월</ResisterPeriod>
+          <ResisterPeriod
+            isClick={isClick === '3개월'}
+            onClick={() => handleDateClick('3개월')}
+          >
+            3개월
+          </ResisterPeriod>
         </ResisterPeriodWrap>
       </TabBottomContainer>
     </TabContainer>
@@ -80,13 +111,24 @@ const TabBanner = styled.div`
 
   display: flex;
   align-items: center;
-  padding-left: 50px;
 
+  padding-left: 20px;
   border-radius: 12px;
   background: #1a2849;
 
   color: ${props => props.theme.colors.white};
   font-size: 17px;
+  gap: 10px;
+`;
+
+const TabBannerTitle = styled.div`
+  font-size: 12px;
+  font-style: normal;
+  margin-bottom: 6px;
+`;
+const TabBannerContent = styled.div`
+  font-size: 17px;
+  font-style: normal;
 `;
 
 const TabNavContainer = styled.div`
@@ -219,9 +261,20 @@ const ResisterPeriodWrap = styled.div`
   align-items: center;
 `;
 
-const ResisterPeriod = styled.div`
+const ResisterPeriodTitle = styled.div`
   color: #a4a4a4;
   font-size: 12px;
   font-weight: 400;
   margin: 0px 5px;
+`;
+
+const ResisterPeriod = styled.div<{ isClick: boolean }>`
+  color: #a4a4a4;
+  font-size: 12px;
+  font-weight: 400;
+  margin: 0px 5px;
+  cursor: pointer;
+
+  color: ${props => (props.isClick ? '#404040' : '#A4A4A4')};
+  font-weight: ${props => (props.isClick ? '700' : '400')};
 `;
