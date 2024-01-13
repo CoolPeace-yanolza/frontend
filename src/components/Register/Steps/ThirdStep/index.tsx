c6682b28d5c364a39f18874f07661e91af6e106a
ovoxiix <ovoxiix@gmail.com>
2024-01-13 23:29:40 +0900

    design: 조건 선택 단계 마크업

--- src/components/Register/Steps/ThirdStep/index.tsx
+++ src/components/Register/Steps/ThirdStep/index.tsx
@@ -1,5 +1,20 @@
+import { InputAccordion } from '@components/Register/common';
+
 const ThirdStep = () => {
-  return <div>ThirdStep</div>;
+  const temp = <div>temp</div>;
+
+  return (
+    <>
+      <InputAccordion
+        description="결제금액에 따라 달라요."
+        content={temp}
+      ></InputAccordion>
+      <InputAccordion
+        description="특정 요일에 사용이 가능해요."
+        content={temp}
+      ></InputAccordion>
+    </>
+  );
 };
 
 export default ThirdStep;
