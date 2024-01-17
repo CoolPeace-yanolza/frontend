4e2551ecc1435b5bf9e1ac3afd5ad1f6231a7d8a
김다빈 <991012dabin@gmail.com>
2024-01-17 17:49:01 +0900

    feat: axios instance 생성

--- src/api/lib/instance.ts
+++ src/api/lib/instance.ts
@@ -1,5 +1,10 @@
-const instance = () => {
-  return 'instance';
-};
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  headers: {
+    'content-type': import.meta.env.VITE_CONTENT_TYPE
+  }
+});
 
 export default instance;
