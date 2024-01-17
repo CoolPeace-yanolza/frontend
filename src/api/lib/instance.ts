78aa5c69a04f9780bcb24340eb959da8c88500e4
김다빈 <991012dabin@gmail.com>
2024-01-17 18:26:04 +0900

    modify: content-type 첫글자 대문자로 변경

--- src/api/lib/instance.ts
+++ src/api/lib/instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   headers: {
-    'content-type': import.meta.env.VITE_CONTENT_TYPE
+    'Content-Type': import.meta.env.VITE_CONTENT_TYPE
   }
 });
 
