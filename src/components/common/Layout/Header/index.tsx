6737d22aa940ae4d8be8abb7246ebc351bca26b6
JitHoon <imessi0336@gmail.com>
2024-01-18 16:34:41 +0900

    design: 헤더 모바일 반응형 추가

--- src/components/common/Layout/Header/index.tsx
+++ src/components/common/Layout/Header/index.tsx
@@ -3,8 +3,8 @@ import styled from '@emotion/styled';
 
 import logo from '@assets/icons/ic-logo.svg';
 import Select from './Select';
-import theme from '@styles/theme';
 import User from './User';
+import theme from '@styles/theme';
 
 const Header = () => {
   return (
@@ -19,6 +19,7 @@ const Header = () => {
         />
       </LogoLink>
       <Buttons>
+        <div></div>
         <Select />
         <User />
       </Buttons>
@@ -38,25 +39,46 @@ const Container = styled.header`
 
   border-radius: 20px;
   padding: 20px;
-  padding-left: 30px;
 
   display: flex;
-  align-items: center;
   justify-content: space-between;
+  align-items: center;
 
   background-color: ${theme.colors.white};
+
+  @media screen and (max-width: 1200px) {
+    height: 65px;
+
+    border-bottom: 1px solid #e6e6e6;
+    border-radius: 0;
+  }
 `;
 
 const LogoLink = styled(NavLink)`
   width: 140px;
   height: 30px;
+
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
 `;
 
 const LogoIcon = styled.img`
   width: 100%;
   height: 100%;
+
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
 `;
 
 const Buttons = styled.div`
   display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 1200px) {
+    width: 100%;
+
+    justify-content: space-between;
+  }
 `;
