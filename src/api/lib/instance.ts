3fa1ec4a231f8567b3d3b07b2f6a43e076402f1a
김다빈 <991012dabin@gmail.com>
2024-01-18 15:50:25 +0900

    feat: axios request interceptor 추가

--- src/api/lib/instance.ts
+++ src/api/lib/instance.ts
@@ -1,3 +1,4 @@
+import { getCookies } from '@utils/lib/cookies';
 import axios from 'axios';
 
 const instance = axios.create({
@@ -5,6 +6,25 @@ const instance = axios.create({
   headers: {
     'Content-Type': import.meta.env.VITE_CONTENT_TYPE
   }
+});
+
+// 요청 인터셉터 추가
+instance.interceptors.request.use(
+  (config: any): any => {
+    const accessToken = getCookies('accessToken');
+
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// TODO : 응답 인터셉터 추가
+instance.interceptors.response.use((response: any): any => {
+  return response;
 });
 
 export default instance;
