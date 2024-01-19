b73cb96abab936aca1e8fb905dd6a5c263f760c2
김다빈 <991012dabin@gmail.com>
2024-01-20 00:24:56 +0900

    feat: 비로그인상태에서 privateRoute에 접근했을 때 해당 path를 저장 후 로그인하면 바로 해당 path로 이동 기능 추가

--- src/routes/PrivateRouter/index.tsx
+++ src/routes/PrivateRouter/index.tsx
@@ -1,9 +1,13 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { Layout } from '@components/common';
 import { getCookies } from '@utils/lib/cookies';
 
 const PrivateRouter = () => {
+  const navigate = useNavigate();
+  const pathname = useLocation();
+
   const accessToken = getCookies('accessToken');
   const refreshToken = getCookies('refreshToken');
   const userName = getCookies('userName');
@@ -12,11 +16,14 @@ const PrivateRouter = () => {
   const isLoggedIn =
     !!accessToken && !!refreshToken && !!userName && !!userEmail;
 
-  if (!isLoggedIn) {
-    // HACK : alert창은 추후 변경 예정입니다.
-    alert('로그인이 필요한 기능입니다.');
-    return <Navigate to="/login" />;
-  }
+  useEffect(() => {
+    if (!isLoggedIn) {
+      // HACK : alert창은 추후 변경 예정입니다.
+      alert('로그인이 필요한 기능입니다.');
+      navigate('/login', { state: pathname });
+    }
+  }, []);
+
   return <Layout />;
 };
 
