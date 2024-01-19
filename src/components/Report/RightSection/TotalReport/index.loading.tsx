73edbf98d332803d58f47e9ab150b836158dde1e
JitHoon <imessi0336@gmail.com>
2024-01-20 00:23:05 +0900

    feat: 총 누적 쿠폰 현황 api 로딩 및 에러 구 현

---
+++ src/components/Report/RightSection/TotalReport/index.loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <h1>누적 똑똑 현황 리포트 로딩중!</h1>;
+};
+
+export default Loading;
--- src/components/Report/RightSection/index.tsx
+++ src/components/Report/RightSection/index.tsx
@@ -1,12 +1,26 @@
+import { Suspense } from 'react';
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import styled from '@emotion/styled';
 
 import TotalReport from './TotalReport';
 import Catchphrase from './Catchphrase';
+import Loading from './TotalReport/index.loading';
+import ErrorFallback from './TotalReport/index.error';
 
 const RightSection = () => {
+  const { reset } = useQueryErrorResetBoundary();
+
   return (
     <Section>
-      <TotalReport />
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={ErrorFallback}
+      >
+        <Suspense fallback={<Loading />}>
+          <TotalReport />
+        </Suspense>
+      </ErrorBoundary>
       <Catchphrase />
     </Section>
   );
