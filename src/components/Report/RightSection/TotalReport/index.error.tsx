6ea180a472500b44920bda738f5a1b27a6991b44
JitHoon <imessi0336@gmail.com>
2024-01-20 03:30:10 +0900

    feat: 총 누적 쿠폰 현황 에러 컴포넌트 구현

--- src/components/Report/RightSection/TotalReport/index.error.tsx
+++ src/components/Report/RightSection/TotalReport/index.error.tsx
@@ -1,15 +1,35 @@
 import { FallbackProps } from 'react-error-boundary';
+import styled from '@emotion/styled';
 
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  console.log(error);
+
   return (
-    <div>
-      <div>
-        <span>누적 똑똑 현황 에러 발생</span>
-        <span>{error.message}</span>
-        <button onClick={resetErrorBoundary}>다시 시도</button>
-      </div>
-    </div>
+    <Container>
+      <ErrorMessage>누적 똑똑 현황 에러 발생</ErrorMessage>
+      <RetryButton onClick={resetErrorBoundary}>다시 시도</RetryButton>
+    </Container>
   );
 };
 
 export default ErrorFallback;
+
+const Container = styled.div`
+  width: 100%;
+  height: 580px;
+
+  padding: 30px 15px;
+  border-radius: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  background-color: white;
+`;
+
+const ErrorMessage = styled.span``;
+
+const RetryButton = styled.button``;
