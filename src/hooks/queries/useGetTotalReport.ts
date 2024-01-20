c4dbc68cc392d856e40df20e4bbbc14a266802cf
JitHoon <imessi0336@gmail.com>
2024-01-21 00:53:03 +0900

    modify: 기존 query hook 파일 컨벤션 적용

--- src/hooks/queries/useGetTotalReport.ts
+++ src/hooks/queries/useGetTotalReport.ts
@@ -1,8 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import getTotalReport from 'src/api/lib/getTotalReport';
+import { getTotalReport } from 'src/api';
 
-export const useGetTotalReport = (accommodation_id: number) =>
+const useGetTotalReport = (accommodation_id: number) =>
   useSuspenseQuery({
     queryKey: ['TotalReport', accommodation_id],
     queryFn: () => getTotalReport(accommodation_id)
   });
+
+export default useGetTotalReport;
