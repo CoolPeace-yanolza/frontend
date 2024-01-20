a97b02ad89854273d647238ba1973f60d99169f7
JitHoon <imessi0336@gmail.com>
2024-01-21 00:56:01 +0900

    feat: 누적 연도 리포트 api 요청 성공 타입 지정

--- src/types/report.ts
+++ src/types/report.ts
@@ -14,3 +14,16 @@ export type TotalReportResult = {
   coupon_total_used_count: number;
   coupon_total_download_count: number;
 };
+
+type CouponSalesItem = {
+  statistics_month: string;
+  total_sales: string;
+  coupon_total_sales: string;
+};
+
+export type YearReportResult = {
+  coupon_total_sales: string;
+  coupon_use_sales: string;
+  coupon_total_used_count: string;
+  coupon_sales_list: CouponSalesItem[];
+};
