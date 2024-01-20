31db78dd22926355038e6dd6fab038a562ac2067
jiohjung98 <ckaquz98@naver.com>
2024-01-20 21:58:42 +0900

    remove: 테스트 콘솔 코드 제거

--- src/components/Settlements/SettlementsLeft/SettlementsSetting/index.tsx
+++ src/components/Settlements/SettlementsLeft/SettlementsSetting/index.tsx
@@ -33,10 +33,8 @@ const SettlementsSetting = () => {
   };
 
   const handleButtonClick = () => {
-    console.log('조회하기 버튼이 클릭되었습니다.');
     if (startDate && endDate) {
         setSettlementsDate({ startDate, endDate });
-        console.log(settlementsDateState);
     }
   };
 
