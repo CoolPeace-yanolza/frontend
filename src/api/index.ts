d418c6b3faf8611ab8010721459592ba26c8c590
JitHoon <imessi0336@gmail.com>
2024-01-21 00:51:20 +0900

    feat: 누적 연도 리포트 api queryFn 생성

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export { default as instance } from './lib/instance';
 export { default as postLogin } from './lib/postLogin';
 export { default as postRefreshToken } from './lib/postRefreshToken';
 export { default as getTotalReport } from './lib/getTotalReport';
+export { default as getYearReport } from './lib/getYearReport';
