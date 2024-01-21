b8c2e49dd6bfcc3088831bab1a1aa32b145e5f2d
김다빈 <991012dabin@gmail.com>
2024-01-22 04:05:45 +0900

    feat: SignUpForm에 react-hook-form 적용

--- src/components/SignUp/SignUpForm/index.tsx
+++ src/components/SignUp/SignUpForm/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { FormProvider, useForm } from 'react-hook-form';
 
 import { SignUpInputValidation } from '@/types/signUp';
 import {
@@ -8,84 +9,88 @@ import {
 } from '@components/common/Auth';
 
 const SignUpForm = () => {
+  const methods = useForm();
+
   const isInvalid = true;
   const isEmailValidationVisible = false;
 
   return (
-    <Form>
-      <InputLabelWrapper>
-        <Label htmlFor="user_name">이름</Label>
-        <AuthInputNormal
-          type="text"
-          id="user_name"
-          placeholder="이름 입력"
-          usedFor="signup"
-          isInvalid={isInvalid}
-        />
-        {isInvalid && (
-          <ValidationText $isInvalid={isInvalid}>
-            이름을 입력해 주세요.
-          </ValidationText>
-        )}
-      </InputLabelWrapper>
-      <InputLabelWrapper>
-        <Label htmlFor="user_email">이메일</Label>
-        <EmailInputWrapper>
-          <EmailInput
-            type="email"
-            id="user_email"
-            placeholder="이메일 입력"
+    <FormProvider {...methods}>
+      <Form>
+        <InputLabelWrapper>
+          <Label htmlFor="user_name">이름</Label>
+          <AuthInputNormal
+            type="text"
+            id="user_name"
+            placeholder="이름 입력"
+            usedFor="signup"
+            isInvalid={isInvalid}
           />
-          <AuthButton
-            size="small"
-            variant="disabled"
-            text="중복확인"
-            buttonFunc={() => {
-              // TODO : 회원가입 API 요청 로직
-            }}
+          {isInvalid && (
+            <ValidationText $isInvalid={isInvalid}>
+              이름을 입력해 주세요.
+            </ValidationText>
+          )}
+        </InputLabelWrapper>
+        <InputLabelWrapper>
+          <Label htmlFor="user_email">이메일</Label>
+          <EmailInputWrapper>
+            <EmailInput
+              type="email"
+              id="user_email"
+              placeholder="이메일 입력"
+            />
+            <AuthButton
+              size="small"
+              variant="disabled"
+              text="중복확인"
+              buttonFunc={() => {
+                // TODO : 회원가입 API 요청 로직
+              }}
+            />
+          </EmailInputWrapper>
+          {isEmailValidationVisible && (
+            <ValidationText $isInvalid={!isInvalid}>
+              사용 가능한 아이디입니다.
+            </ValidationText>
+          )}
+        </InputLabelWrapper>
+        <InputLabelWrapper>
+          <Label htmlFor="user_password">비밀번호</Label>
+          <AuthInputPassword
+            id="user_password"
+            placeholder="8-20자, 영문/숫자/특수문자 조합"
+            usedFor="signup"
+            isInvalid={isInvalid}
           />
-        </EmailInputWrapper>
-        {isEmailValidationVisible && (
-          <ValidationText $isInvalid={!isInvalid}>
-            사용 가능한 아이디입니다.
-          </ValidationText>
-        )}
-      </InputLabelWrapper>
-      <InputLabelWrapper>
-        <Label htmlFor="user_password">비밀번호</Label>
-        <AuthInputPassword
-          id="user_password"
-          placeholder="8-20자, 영문/숫자/특수문자 조합"
-          usedFor="signup"
-          isInvalid={isInvalid}
+          {isInvalid && (
+            <ValidationText $isInvalid={isInvalid}>
+              비밀번호 형식이 아닙니다.
+            </ValidationText>
+          )}
+        </InputLabelWrapper>
+        <InputLabelWrapper>
+          <Label htmlFor="user_password_confirm">비밀번호 확인</Label>
+          <AuthInputPassword
+            id="user_password_confirm"
+            placeholder="다시 한번 입력해주세요."
+            usedFor="signup"
+            isInvalid={isInvalid}
+          />
+          {isInvalid && (
+            <ValidationText $isInvalid={isInvalid}>
+              비밀번호가 일치하지 않습니다.
+            </ValidationText>
+          )}
+        </InputLabelWrapper>
+        <AuthButton
+          size="large"
+          variant="navy"
+          text="회원가입"
+          buttonFunc={() => {}}
         />
-        {isInvalid && (
-          <ValidationText $isInvalid={isInvalid}>
-            비밀번호 형식이 아닙니다.
-          </ValidationText>
-        )}
-      </InputLabelWrapper>
-      <InputLabelWrapper>
-        <Label htmlFor="user_password_confirm">비밀번호 확인</Label>
-        <AuthInputPassword
-          id="user_password_confirm"
-          placeholder="다시 한번 입력해주세요."
-          usedFor="signup"
-          isInvalid={isInvalid}
-        />
-        {isInvalid && (
-          <ValidationText $isInvalid={isInvalid}>
-            비밀번호가 일치하지 않습니다.
-          </ValidationText>
-        )}
-      </InputLabelWrapper>
-      <AuthButton
-        size="large"
-        variant="navy"
-        text="회원가입"
-        buttonFunc={() => {}}
-      />
-    </Form>
+      </Form>
+    </FormProvider>
   );
 };
 
