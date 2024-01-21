65fd1f8929f9548d67a3826001ed91785d06cdcd
JitHoon <imessi0336@gmail.com>
2024-01-21 21:28:07 +0900

    feat: 레이아웃, 누적 리포트, api, react-query 비동기 처리 타입 지정

--- src/api/lib/getHeaderAccommodation.ts
+++ src/api/lib/getHeaderAccommodation.ts
@@ -1,8 +1,11 @@
+import { AxiosResponse } from 'axios';
+
 import { instance } from '..';
 import { HeaderAccommodationResult } from '@/types/layout';
 
 const getHeaderAccommodation = async (): Promise<HeaderAccommodationResult> => {
-  const response = await instance.get(`/v1/accommodation`);
+  const response: AxiosResponse<HeaderAccommodationResult, Error> =
+    await instance.get(`/v1/accommodation`);
 
   return response.data;
 };
--- src/api/lib/getTotalReport.ts
+++ src/api/lib/getTotalReport.ts
@@ -1,10 +1,12 @@
+import { AxiosResponse } from 'axios';
+
 import { instance } from '..';
 import { TotalReportResult } from '@/types/report';
 
 const getTotalReport = async (
   accommodation_id: number
 ): Promise<TotalReportResult> => {
-  const response = await instance.get(
+  const response: AxiosResponse<TotalReportResult, Error> = await instance.get(
     `/v1/dashboards/${accommodation_id}/reports/total`
   );
 
