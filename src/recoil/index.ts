f3082d9992f1b8b150c9d0ff8848ca7eb90cbb11
JitHoon <imessi0336@gmail.com>
2024-01-22 15:45:35 +0900

    feat: 누적 리포트 연도 select 리코일 atom 생성

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,2 +1,3 @@
 export { default as headerAccommodationState } from './atoms/headerAccommodationState';
 export { default as couponListState } from './atoms/couponListState';
+export { default as selectedYearState } from './atoms/selectedYearState';
