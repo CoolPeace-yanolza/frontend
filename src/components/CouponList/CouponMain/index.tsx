03bdf937edb619aee74dc000a5ffa5a98dfc48af
jinjoo-jung <wjdwlswn23@naver.com>
2024-01-23 03:04:13 +0900

    feat: 최근 등록일 기준 나열 (임시)

--- src/components/CouponList/CouponMain/index.tsx
+++ src/components/CouponList/CouponMain/index.tsx
@@ -11,7 +11,16 @@ import couponListState from '@recoil/atoms/couponListState';
 
 const CouponMain = () => {
   const coupons = useRecoilValue(couponListState);
-  console.log('recoil로 관리되는 쿠폰 리스트 ', coupons);
+
+  // // 최근 등록일 기준으로 나열
+  // const sortedCoupons = coupons?.content
+  //   ? [...coupons.content].sort((a, b) => {
+  //       const dateA = new Date(a.created_date).getTime();
+  //       const dateB = new Date(b.created_date).getTime();
+  //       return dateB - dateA;
+  //     })
+  //   : [];
+  // console.log('recoil로 관리되는 쿠폰 리스트 ', coupons);
 
   return (
     <MainContainer>
