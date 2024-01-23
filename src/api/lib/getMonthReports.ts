7bd6a48d8be4fa1905ef07ca758a32521a6fa51a
TeukHee <rlaxmrgml@naver.com>
2024-01-23 16:05:35 +0900

    modify: 월별차트 변경된 응답 데이터형식 반영

--- src/api/lib/getMonthReports.ts
+++ src/api/lib/getMonthReports.ts
@@ -4,7 +4,7 @@ import { MonthReportsResults } from '@/types/dashboard';
 
 const getMonthReports = async (id: number): Promise<MonthReportsResults[]> => {
   const response = await instance.get(`v1/dashboards/${id}/reports/month`);
-  return response.data;
+  return response.data.monthly_data_responses;
 };
 
 export default getMonthReports;
