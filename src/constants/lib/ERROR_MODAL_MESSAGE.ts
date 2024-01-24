819c0e6477691ee865dcfe2f8274714872c95140
JitHoon <imessi0336@gmail.com>
2024-01-24 23:56:43 +0900

    feat: 모달 에러 메시지 상수화

---
+++ src/constants/lib/ERROR_MODAL_MESSAGE.ts
@@ -0,0 +1,12 @@
+const ERROR_MODAL_MESSAGE = {
+  LOGIN: {
+    text: '이메일 또는 비밀번호를 잘못 입력했습니다.',
+    errorText: '입력하신 내용을 다시 확인해주세요.'
+  },
+  USER_MODAL: {
+    text: '로그아웃에 실패하였습니다.',
+    errorText: '잠시후 재시도 해주세요.'
+  }
+};
+
+export default ERROR_MODAL_MESSAGE;
