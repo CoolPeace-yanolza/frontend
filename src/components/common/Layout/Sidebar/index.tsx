0b56f72c09c7d14a06d5500ba7bcf81eb28bac8a
JitHoon <imessi0336@gmail.com>
2024-01-24 18:29:00 +0900

    feat: 사이드바에서 url 이동 시 사이드바 자동 닫힘 기능 추가

--- src/components/common/Layout/Sidebar/index.tsx
+++ src/components/common/Layout/Sidebar/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import { useLocation } from 'react-router-dom';
 
 import Header from './Header';
 import Navigation from './Navigation';
@@ -8,6 +9,18 @@ import theme from '@styles/theme';
 
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const location = useLocation();
+  const locationPathname = location.pathname;
+  const [prevLocation, setPrevLocation] = useState('');
+
+  // 링크 이동 시 사이드바 닫힘
+  useEffect(() => {
+    if (prevLocation !== locationPathname) setIsSidebarOpen(false);
+  }, [prevLocation, locationPathname]);
+
+  useEffect(() => {
+    setPrevLocation(locationPathname);
+  }, [locationPathname]);
 
   return (
     <>
