4edc56f3e5f24f94e1a0e6d5de272fe6d755a0bf
한은지 <155057856+ovoxiix@users.noreply.github.com>
2024-01-26 20:12:06 +0900

    fix: 빌드 에러 수정

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -4,4 +4,3 @@ export { default as MobileDashboardHeader } from './MobileDashboardHeader';
 export { default as Backdrop } from './Backdrop';
 export { default as Footer } from './Footer';
 export { default as ErrorModal } from './ErrorModal';
-export { default as Backdrop } from './Backdrop';
