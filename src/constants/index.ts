3a60ba865eee5a7801c87f59c99f4e22c22ff278
jiohjung98 <ckaquz98@naver.com>
2024-01-27 04:57:32 +0900

    modify: SETTLEMENTS_TABLE_KEY 변수명 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as STATUS_CODES } from './lib/STATUS_CODES';
 export { default as ERROR_MODAL_MESSAGE } from './lib/ERROR_MODAL_MESSAGE';
 export { default as SORT_OPTION } from './lib/SORT_OPTION';
+export { default as SETTLEMENTS_TABLE_KEY } from './lib/SETTLEMENTS_TABLE_KEY';
