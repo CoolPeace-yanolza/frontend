0860a243a14cf5735b0d5ad7eb0fee39cd0471d5
jiohjung98 <ckaquz98@naver.com>
2024-01-26 18:58:15 +0900

    modify: 드롭다운 순서 변경

--- src/components/Settlements/SettlementsLeft/SettlementsSetting/Settlemented/index.tsx
+++ src/components/Settlements/SettlementsLeft/SettlementsSetting/Settlemented/index.tsx
@@ -28,9 +28,9 @@ const Settlemented = () => {
   const itemsPerPage = 10;
 
   const sortOptions = [
-    { key: 'amountDesc', text: '정산금액 많은 순', value: 'amountDesc' },
-    { key: 'dateDesc', text: '정산 완료일 최근 순', value: 'dateDesc' },
     { key: 'couponDateDesc', text: '쿠폰 사용일 최근 순', value: 'couponDateDesc' },
+    { key: 'dateDesc', text: '정산 완료일 최근 순', value: 'dateDesc' },
+    { key: 'amountDesc', text: '정산금액 많은 순', value: 'amountDesc' },
     { key: 'usageCountDesc', text: '사용건 많은 순', value: 'usageCountDesc' },
   ];
 
