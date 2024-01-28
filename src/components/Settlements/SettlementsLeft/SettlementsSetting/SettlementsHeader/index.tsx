c6d2fabff331aa4ee9ed89029eafe4da67981e74
jiohjung98 <ckaquz98@naver.com>
2024-01-28 17:20:04 +0900

    feat: 팝업  클릭 시 팝업 닫기

--- src/components/Settlements/SettlementsLeft/SettlementsSetting/SettlementsHeader/index.tsx
+++ src/components/Settlements/SettlementsLeft/SettlementsSetting/SettlementsHeader/index.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import settlementsAdminIcon from '@assets/icons/settlemented-admin.svg';
 import informationIcon from '@assets/icons/information-circle-outline.svg';
@@ -7,11 +7,25 @@ import SettlementsPopup from './SettlementsPopup';
 import theme from '@styles/theme';
 
 const SettlementsHeader = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false)
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   const handlePopupToggle = () => {
     setIsPopupOpen(!isPopupOpen);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (isPopupOpen && ref.current && !ref.current.contains(event.target as Node)) {
+        setIsPopupOpen(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isPopupOpen]);
 
   return (
     <Container>
@@ -27,7 +41,7 @@ const SettlementsHeader = () => {
         <MiddleText>
             쿠폰 프로모션에 적용한 정산 내역을 확인할 수 있습니다.
         </MiddleText>
-        <InformationContainer>
+        <InformationContainer ref={ref}>
         <InformationIcon
          src={informationIcon}
          alt="정보"
